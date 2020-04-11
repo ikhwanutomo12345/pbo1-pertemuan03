227045e093b5520ace3f2de641afe4f04324258e
ikhwanutomo12345 <ikhwanutomo12345@gmail.com>
2020-04-11 09:19:39 +0700

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -8,4 +8,14 @@ public class OperatorBinary {
     String nama = " Ikhwan Utomo ";
     int jumlah = 24;
     double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9+2);
+
+    }
 }
