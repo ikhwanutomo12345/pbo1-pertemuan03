27597d94b2afbb2e87c04d78ceb61f3cfc5b31a8
ikhwanutomo12345 <ikhwanutomo12345@gmail.com>
2020-04-11 09:15:01 +0700

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = " Ikhwan Utomo ";
+    int jumlah = 24;
+    double indeksPrestasiKumulatif = 3.89;
+}
