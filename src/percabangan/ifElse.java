901633c37cace9075b4901361c2c27d4a1a35e06
ikhwanutomo12345 <ikhwanutomo12345@gmail.com>
2020-04-12 09:18:50 +0700

    Operator Percabangan

---
+++ src/percabangan/ifElse.java
@@ -0,0 +1,14 @@
+package percabangan;
+
+public class ifElse {
+    public static void main(String[] args) {
+        double totalBelanja = 25000;
+        double uangDidompet = 25000;
+
+        if (uangDidompet<totalBelanja) {
+            System.out.println("Uang kurang, Kurangi jajan anda");
+        }else{
+            System.out.println("Pas Uang Pian nang ai,berelaan sanakkkkkkk");
+        }
+    }
+}
