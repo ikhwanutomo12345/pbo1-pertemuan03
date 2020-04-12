d37b7fb08a6d64aaf6a25b2724fe17252149b482
ikhwanutomo12345 <ikhwanutomo12345@gmail.com>
2020-04-12 09:50:30 +0700

    Operator Switch2

---
+++ src/percabangan/Switch2.java
@@ -0,0 +1,28 @@
+package percabangan;
+
+public class Switch2 {
+    public static void main(String[] args) {
+        char nilai='A';
+        switch (nilai){
+            case 'A':
+                System.out.println("Mhs :\"Pak nilai ulun berapa\"");
+                System.out.println("Dosen :\"ANDA DAPAT NILAI A SANAK\"");
+                break;
+            case 'B':
+                System.out.println("Mhs :\"Kenapa Ulun kada dapat nilai A Pak?\"");
+                System.out.println("Dosen :\"AOWKOWKWWOK\"");
+                break;
+            case 'C':
+                System.out.println("Mhs :\"Ulun turun tarus pak ai,tugas ulun gawi tarus\"");
+                System.out.println("Dosen :\"Tapi ujian kawa lah menjawab\"");
+                System.out.println("Mhs :\"AKWKKAKWKWKWKWKWKW\"");
+                break;
+           default:
+               System.out.println("Mhs :\"Ulun turun tarus pak ai,tugas ulun gawi tarus\"");
+               System.out.println("Dosen :\"NANIIIIII Bujur jua kah\"");
+               System.out.println("Dosen :\"MEMERIKSA BERKAS\"");
+               System.out.println("Dosen :\"MENCELENG !!!!!\"");
+               System.out.println("Mhs :\"KABORRRRRRRR\"");
+        }
+    }
+}
